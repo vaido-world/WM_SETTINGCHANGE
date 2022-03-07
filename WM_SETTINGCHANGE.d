370ebed29bb771feda44afaefa24d610048a63ef
Feldwor <BoQsc@users.noreply.github.com>
2022-03-07 12:24:12 +0200

    Update WM_SETTINGCHANGE.d

--- WM_SETTINGCHANGE.d
+++ WM_SETTINGCHANGE.d
@@ -24,19 +24,19 @@ import std.string: isNumeric;
             case 2:
                 return broadcastSettingChange(args[1]);
                 break;
-               
+                
+            case 3:
+                string argument2 = args[2].replaceAll(regex(r"[^0-9.]","g"), "");
+                if (argument2.isNumeric){
+                    return broadcastSettingChange(args[1], std.conv.to!uint(argument2));
+                } else {
+                    writeln("Invalid timeout value: ", args[2]," (must be uint integer)");
+                }
+                break;       
+
             default:
-                case 3:
-                    string argument2 = args[2].replaceAll(regex(r"[^0-9.]","g"), "");
-                    if (argument2.isNumeric){
-                        return broadcastSettingChange(args[1], std.conv.to!uint(argument2));
-                    } else {
-                        writeln("Invalid timeout value: ", args[2]," (must be uint integer)");
-                    }
-                    break;       
-
-
-
+                goto case 3;
+                break;
         }
     } else {
     
