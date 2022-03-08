17309087cab22a0f7228e03f310d8f750e6d1e9d
Feldwor <BoQsc@users.noreply.github.com>
2022-03-08 11:21:22 +0200

    split commandline interface into source code file

--- WM_SETTINGCHANGE.d
+++ WM_SETTINGCHANGE.d
@@ -12,40 +12,6 @@ import core.sys.windows.basetsd : PDWORD_PTR;
 
 import std.stdio;
 
-import std.conv;
-import std.regex;
-import std.string: isNumeric;
-
- int main(string[] args){
-
-    if (args.length > 1) {
-        switch (args.length){
-        
-            case 2:
-                return broadcastSettingChange(args[1]);
-                break;
-                
-            case 3:
-                string argument2 = args[2].replaceAll(regex(r"[^0-9.]","g"), "");
-                if (argument2.isNumeric){
-                    return broadcastSettingChange(args[1], std.conv.to!uint(argument2));
-                } else {
-                    writeln("Invalid timeout value: ", args[2]," (must be uint integer)");
-                }
-                break;       
-
-            default:
-                goto case 3;
-                break;
-        }
-    } else {
-    
-        return broadcastSettingChange();
-    }
-
-    return 1;
-
- }
 
  /**
     Broadcasts a signal to update All Environment variables On Windows XP, 7, 8, 10 systems.
@@ -156,7 +122,7 @@ __________________INFORMATIVE_WARNINGS__________________:
     switch (lastError){
     
         case ERROR_SUCCESS:
-            writeln(__FILE__, ": Success. Sent a WM_SETTINGCHANGE of ", broadcastAddress, ", broadcast to all top windows.");
+            writeln("[", __FILE__, "] Success. Sent a WM_SETTINGCHANGE of ", broadcastAddress, ", broadcast to all top windows.");
             break;
         
         case ERROR_TIMEOUT:
---
+++ command_line_interface.d
@@ -0,0 +1,37 @@
+import WM_SETTINGCHANGE;
+
+import std.stdio;
+import std.regex;
+import std.string: isNumeric;
+import std.conv;
+
+int main(string[] args){
+
+    if (args.length > 1) {
+        switch (args.length){
+        
+            case 2:
+                return broadcastSettingChange(args[1]);
+                break;
+                
+            case 3:
+                string argument2 = args[2].replaceAll(regex(r"[^0-9.]","g"), "");
+                if (argument2.isNumeric){
+                    return broadcastSettingChange(args[1], std.conv.to!uint(argument2));
+                } else {
+                    writeln("Invalid timeout value: ", args[2]," (must be uint integer)");
+                }
+                break;       
+
+            default:
+                goto case 3;
+                break;
+        }
+    } else {
+    
+        return broadcastSettingChange();
+    }
+
+    return 1;
+
+ }
